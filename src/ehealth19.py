d758ad08539921bab7ed353c4892cb874bb8af1d
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2020-04-02 22:16:14 +0000

    Implement model training per entity label

--- src/ehealth19.py
+++ src/ehealth19.py
@@ -6,7 +6,7 @@ from torch.nn import CrossEntropyLoss
 from tqdm import tqdm
 
 from scripts.submit import Algorithm, Run, handle_args
-from scripts.utils import Collection, Keyphrase, Relation
+from scripts.utils import ENTITIES, RELATIONS, Collection, Keyphrase, Relation
 
 
 class UHMajaModel(Algorithm):
@@ -15,7 +15,7 @@ class UHMajaModel(Algorithm):
     TOKEN_REPR_DIM = 300
 
     def __init__(self):
-        self.model_taskA = None
+        self.taskA_models = {}
 
     def run(self, collection: Collection, *args, taskA: bool, taskB: bool, **kargs):
         if taskA:
@@ -24,38 +24,56 @@ class UHMajaModel(Algorithm):
             self.run_taskB(collection, *args, **kargs)
         return collection
 
-    def run_taskA(self, collection: Collection, **kargs):
-        idx = 0
-        model = self.model_taskA
+    def run_taskA(self, collection: Collection, *args, **kargs):
+        for label in ENTITIES:
+            self.run_taskA_for_label(collection, label, *args, **kargs)
+        collection.fix_ids()
+
+    def run_taskA_for_label(
+        self, collection: Collection, entity_label: str, *args, **kargs
+    ):
+        model = self.taskA_models[entity_label]
         dataset = BILUOVSentencesDS([s.text for s in collection.sentences])
 
-        for sid, (*s_features, label) in tqdm(
-            enumerate(dataset.shallow_dataloader()), total=len(dataset)
+        for sid, (*s_features, _) in tqdm(
+            enumerate(dataset.shallow_dataloader()),
+            total=len(dataset),
+            desc=entity_label,
         ):
-            sentence = collection.sentences[sid]
             tokensxsentence = dataset.tokensxsentence[sid]
             output = model(s_features).squeeze(0)
             output = output.argmax(dim=-1)
             labels = [dataset.labels[x] for x in output]
             decoded = from_biluov(labels, tokensxsentence, spans=True)
+
+            sentence = collection.sentences[sid]
             for spans in decoded:
-                # TODO: set Keyphrase label
-                keyphrase = Keyphrase(sentence, None, idx, spans)
-                idx += 1
+                keyphrase = Keyphrase(sentence, entity_label, -1, spans)
                 sentence.keyphrases.append(keyphrase)
 
-    def run_taskB(self, collection: Collection, **kargs):
+    def run_taskB(self, collection: Collection, *args, **kargs):
         pass
 
     def train(self, collection: Collection, n_epochs=100):
         self.model = self.train_taskA(collection, n_epochs)
 
     def train_taskA(self, collection: Collection, n_epochs=100):
+        for label in ENTITIES:
+            self.taskA_models[label] = self.build_taskA_model(
+                collection, label, n_epochs
+            )
+
+    def build_taskA_model(
+        self, collection: Collection, entity_label: str, n_epochs=100
+    ):
         sentences = [s.text for s in collection.sentences]
-        entities = [[k.spans for k in s.keyphrases] for s in collection.sentences]
+        entities = [
+            [k.spans for k in s.keyphrases if k.label == entity_label]
+            for s in collection.sentences
+        ]
         dataset = BILUOVSentencesDS(sentences, entities)
 
-        self.model_taskA = model = BasicSequenceTagger(
+        model = BasicSequenceTagger(
             char_vocab_size=len(dataset.char_vocab),
             char_embedding_dim=self.CHAR_EMBEDDING_DIM,
             padding_idx=dataset.PADDING,
@@ -75,7 +93,9 @@ class UHMajaModel(Algorithm):
             running_loss = 0.0
 
             for data in tqdm(
-                dataset.shallow_dataloader(shuffle=True), total=len(dataset)
+                dataset.shallow_dataloader(shuffle=True),
+                total=len(dataset),
+                desc=entity_label,
             ):
                 *sentence, label = data
                 optimizer.zero_grad()
