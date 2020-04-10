9e3c37bd31635d31f6a061f6140de00ffc8c0c36
Juan Pablo Consuegra Ayala <jpconsuegra93@gmail.com>
2020-04-10 15:18:08 +0000

    Finish train_taskB

--- src/ehealth19.py
+++ src/ehealth19.py
@@ -1,7 +1,15 @@
 import torch
 import torch.optim as optim
-from kdtools.datasets import BILUOVSentencesDS, from_biluov
-from kdtools.models import BasicSequenceTagger
+from kdtools.datasets import (
+    BILUOVSentencesDS,
+    DependencyTreeDS,
+    from_biluov,
+    get_nlp,
+    match_tokens_to_entities,
+)
+from kdtools.encoders import SequenceCharEncoder
+from kdtools.layers import CharEmbeddingEncoder
+from kdtools.models import BasicSequenceTagger, BasicSequenceClassifier
 from kdtools.utils import (
     jointly_train_on_shallow_dataloader,
     train_on_shallow_dataloader,
@@ -19,7 +27,9 @@ class UHMajaModel(Algorithm):
     TOKEN_REPR_DIM = 300
 
     def __init__(self):
-        self.taskA_models = {}
+        self.nlp = get_nlp()
+        self.taskA_models: dict = None
+        self.taskB_model = None
 
     def run(self, collection: Collection, *args, taskA: bool, taskB: bool, **kargs):
         if taskA:
@@ -37,7 +47,9 @@ class UHMajaModel(Algorithm):
         self, collection: Collection, entity_label: str, *args, **kargs
     ):
         model = self.taskA_models[entity_label]
-        dataset = BILUOVSentencesDS([s.text for s in collection.sentences])
+        dataset = BILUOVSentencesDS(
+            [s.text for s in collection.sentences], language=self.nlp
+        )
 
         for sid, (*s_features, _) in tqdm(
             enumerate(dataset.shallow_dataloader()),
@@ -58,8 +70,9 @@ class UHMajaModel(Algorithm):
     def run_taskB(self, collection: Collection, *args, **kargs):
         pass
 
-    def train(self, collection: Collection, *, jointly, n_epochs=100):
+    def train(self, collection: Collection, *, jointly, inclusion, n_epochs=100):
         self.train_taskA(collection, jointly, n_epochs)
+        self.train_taskB(collection, jointly, inclusion, n_epochs)
 
     def train_taskA(self, collection: Collection, jointly, n_epochs=100):
         char_encoder = None
@@ -95,7 +108,7 @@ class UHMajaModel(Algorithm):
             [k.spans for k in s.keyphrases if k.label == entity_label]
             for s in collection.sentences
         ]
-        dataset = BILUOVSentencesDS(sentences, entities)
+        dataset = BILUOVSentencesDS(sentences, entities, language=self.nlp)
 
         model = BasicSequenceTagger(
             char_vocab_size=dataset.char_size,
@@ -131,6 +144,68 @@ class UHMajaModel(Algorithm):
             desc=desc,
         )
 
+    def train_taskB(self, collection: Collection, jointly, inclusion, n_epochs=100):
+        nlp = self.nlp
+
+        tokensxsentence = [nlp(s.text) for s in collection.sentences]
+        entities = [
+            [(k.spans, k.label) for k in s.keyphrases] for s in collection.sentences
+        ]
+
+        entitiesxsentence, token2label = match_tokens_to_entities(
+            tokensxsentence, entities, only_representative=False
+        )
+
+        keyphrase2tokens = {
+            keyphrase: token
+            for sentence, tokens in zip(collection.sentences, entitiesxsentence)
+            for keyphrase, token in zip(sentence.keyphrases, tokens)
+        }
+        relations = {
+            (
+                keyphrase2tokens[rel.from_phrase],
+                keyphrase2tokens[rel.to_phrase],
+            ): rel.label
+            for sentence in collection.sentences
+            for rel in sentence.relations
+        }
+
+        dataset = DependencyTreeDS(
+            entitiesxsentence,
+            relations,
+            token2label,
+            ENTITIES,
+            RELATIONS,
+            nlp,
+            inclusion=inclusion,
+            char2repr=None,
+        )
+
+        char2repr = (
+            next(iter(self.taskA_models.values())).char_encoder if jointly else None
+        )
+
+        model = BasicSequenceClassifier(
+            char_vocab_size=dataset.char_size,
+            char_embedding_dim=self.CHAR_EMBEDDING_DIM,
+            padding_idx=dataset.padding,
+            char_repr_dim=self.CHAR_REPR_DIM,
+            word_repr_dim=dataset.vectors_len,
+            postag_repr_dim=dataset.pos_size,
+            dep_repr_dim=dataset.dep_size,
+            entity_repr_dim=dataset.ent_size,
+            subtree_repr_dim=self.TOKEN_REPR_DIM,
+            token_repr_dim=self.TOKEN_REPR_DIM,
+            num_labels=dataset.label_size,
+            char_encoder=char2repr,
+            already_encoded=False,
+            freeze=True,
+        )
+
+        train_on_shallow_dataloader(model, dataset, n_epochs=n_epochs, desc="relations")
+
+        self.taskB_model = model
+
 
 if __name__ == "__main__":
     from pathlib import Path
@@ -138,7 +213,7 @@ if __name__ == "__main__":
     algorithm = UHMajaModel()
 
     training = Collection().load(Path("data/training/scenario.txt"))
-    algorithm.train(training, jointly=True)
+    algorithm.train(training, jointly=True, inclusion=0.1)
 
     tasks = handle_args()
     Run.submit("ehealth19-maja", tasks, algorithm)
